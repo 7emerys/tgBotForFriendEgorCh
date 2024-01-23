ec30190b51c7324d93821c6b749d774e6ff01679
7emerys <arsenyi2017431003@gmail.com>
2024-01-23 22:52:33 +0300

    Добавление обратной связи с пользователем и вывод приветствия со смайликом, а так же скрытый токен и id группы в которую приходят сообщения пользователей

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from aiogram import Bot, Dispatcher,executor,types
+from dotenv import load_dotenv
+import os,sys,time
 
-
-bot = Bot('6636165347:AAHz2Zw-B_ibYi46fM1r870AlzsvXxMgmS0')
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
-async def cmd_start(message:types.Message):
+async def cmd_start(message: types.Message):
+    await message.answer_sticker('CAACAgIAAxkBAAMOZbAIb3rH_mjDy3qqud_Soxkw4FcAAqkYAAIjdeBJmzhaCBy9ZUs0BA')
     await message.answer(f"{message.from_user.first_name}, добро пожаловать в KOPOSIARY - VAPE КИРОВ")
 
-
+@dp.message_handler(content_types=['photo','text','document','voice','video','animation','sticker'])
+async def forward_message(message: types.Message):
+    await bot.forward_message(os.getenv('GROUP_ID'), message.from_user.id, message.message_id)
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
-
+    start_time = time.time()
+    executor.start_polling(dp,skip_updates=True)
+    print("--- %s seconds ---" % (time.time() - start_time))
