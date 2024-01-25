c46d5688f2b2d9b3b5f789b87d101a1f3ebe4f4f
Aleksandr <neon4ik378@gmail.com>
2024-01-25 23:48:44 +0300

    Добавил базы данных(недоработанные)

--- app/database.py
+++ app/database.py
@@ -5,17 +5,49 @@ cur = db.cursor()
 
 
 async def db_start():
+    #ПОМЕНЯТЬ
     cur.execute("CREATE TABLE IF NOT EXISTS accounts_user("
                 "user_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 "tg_id INTEGER, "
                 "cart_id TEXT, "
                 "sells_count INTEGER)")
-    cur.execute("CREATE TABLE IF NOT EXISTS items("
-                "i_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+
+    cur.execute("CREATE TABLE IF NOT EXISTS yz("
+                "tg_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                 "name TEXT,"
-                "taste TEXT,"
-                "price INTEGER )")
+                "dop_name TEXT,"
+                "count INTEGER,"
+                "sells_count INTEGER)")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS novovyatsk("
+                "tg_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                "name TEXT,"
+                "dop_name TEXT,"
+                "count INTEGER,"
+                "sells_count INTEGER)")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS cym("
+                "tg_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                "name TEXT,"
+                "dop_name TEXT,"
+                "count INTEGER,"
+                "sells_count INTEGER)")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS every_day_catalog("
+                "№ INTEGER PRIMARY KEY AUTOINCREMENT,"
+                "name TEXT , "
+                "count INTEGER,"
+                "price INTEGER,"
+                "sum INTEGER )")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS catalog("
+                "№ INTEGER PRIMARY KEY AUTOINCREMENT,"
+                "name TEXT , "
+                "count INTEGER,"
+                "price INTEGER,"
+                "sum INTEGER )")
     db.commit()
+
 
 async def cmd_db_start(user_id):
     user = cur.execute("SELECT * FROM accounts_user WHERE tg_id == {key}".format(key=user_id)).fetchone()
